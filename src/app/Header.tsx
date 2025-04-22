d28107f59d879d6bebcae1bcc8776bbc522963c0
yoshizuka <76939507+yosizuka@users.noreply.github.com>
2025-04-23 01:11:43 +0900

    記号をエスケープ文字に変換

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Header = () => {
   return (
     <nav className="h-14 bg-emerald-400/90 text-white flex items-center justify-between space-x-2 px-2 w-full shadow-md bg-gradient-to-br from-blue-500/80 to-purple-600/80 shadow-blue-500/30 backdrop-blur-sm fixed top-0 z-5">
-      <h2 className="ml-4 px-4 py-1 font-semibold text-2xl bg-black rounded">Paquate's works</h2>
+      <h2 className="ml-4 px-4 py-1 font-semibold text-2xl bg-black rounded">Paquate&apos;s works</h2>
       <a className="mr-4" href="https://github.com/paquete-tab/paquete-works" target="_brank"><Image src={"/github-mark.svg"} alt="https://github.com/paquete-tab/paquete-works" width={40} height={40} /></a>
     </nav>
   )
